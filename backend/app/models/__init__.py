bf854533f29fe315189e25344efd48fbf030a294
Iman Joolaie <i.joolaie@gmail.com>
2026-09-07 11:57:34 +0330

    feat(stage8): register workforce proposal model

--- backend/app/models/__init__.py
+++ backend/app/models/__init__.py
@@ -41,6 +41,7 @@ from app.models.agent_runtime_binding import AgentRuntimeBinding
 from app.models.agent_evaluation import AgentEvaluation, AgentEvaluationStatus
 from app.models.agent_identity import AgentIdentity
 from app.models.agent_access_review import AgentAccessReview, AgentAccessReviewDecision
+from app.models.agent_workforce_proposal import AgentWorkforceProposal, AgentWorkforceProposalStatus
 from app.models.test_definition import TestDefinition
 from app.models.test_run import TestRun, TestRunStatus
 from app.models.test_run_artifact import TestRunArtifact
@@ -62,6 +63,6 @@ __all__ = [
     "TenantEntitlement", "SupportEscalation", "CommercialLicense", "WorkItem", "WorkItemStatus", "ExecutorType",
     "AgentDefinition", "AgentTemplate", "AgentTemplateStatus", "AgentInstance", "AgentInstanceStatus", "AgentRuntimeBinding",
     "AgentEvaluation", "AgentEvaluationStatus", "AgentIdentity", "AgentAccessReview", "AgentAccessReviewDecision",
-    "TestDefinition", "TestRun", "TestRunStatus", "TestRunArtifact", "TeamDefinition", "TeamVersion",
-    "TeamInstallation", "TeamEvaluation", "MarketplacePublication",
+    "AgentWorkforceProposal", "AgentWorkforceProposalStatus", "TestDefinition", "TestRun", "TestRunStatus", "TestRunArtifact",
+    "TeamDefinition", "TeamVersion", "TeamInstallation", "TeamEvaluation", "MarketplacePublication",
 ]
