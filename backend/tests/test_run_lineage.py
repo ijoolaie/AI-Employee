2a1e90ecd624eb24f89702be7b09beba72fd3618
Iman Joolaie <i.joolaie@gmail.com>
2026-09-13 12:10:14 +0330

    remove incomplete lineage test scaffold

--- backend/tests/test_run_lineage.py
+++
@@ -1,6 +0,0 @@
-"""Regression coverage for Run lineage governance."""
-
-
-def test_placeholder_for_run_lineage_governance():
-    # Implemented with service integration tests when lineage columns are enabled.
-    assert True
